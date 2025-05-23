6681247cd95c43a99e50a5acd5a600447d7a6967
Alexander Oliva <alexcayd@gmail.com>
2025-05-23 00:44:53 -0600

    Renovación 2025 parte 1

--- app.js
+++ app.js
@@ -23,3 +23,20 @@ function scrollNav() {
         });
     });
 }
+
+// Hover imagen aboutme
+const imageContainer = document.querySelector('.aboutme__imagecontenedor');
+
+imageContainer.addEventListener('mousemove', (e) => {
+  const rect = imageContainer.getBoundingClientRect();
+  const x = ((e.clientX - rect.left) / rect.width) * 100;
+  const y = ((e.clientY - rect.top) / rect.height) * 100;
+  imageContainer.style.setProperty('--x', `${x}%`);
+  imageContainer.style.setProperty('--y', `${y}%`);
+});
+
+imageContainer.addEventListener('mouseleave', () => {
+  imageContainer.style.setProperty('--x', `50%`);
+  imageContainer.style.setProperty('--y', `50%`);
+});
+
