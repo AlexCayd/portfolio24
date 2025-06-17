f7804f8bc0d06ff18a7a3c730aca83f01bbe9eb9
Alex <alexcayd@gmail.com>
2025-06-16 20:10:43 -0600

    Listo para trabajar en paginas de proyectos

--- app.js
+++ app.js
@@ -14,14 +14,14 @@ function scrollNav() {
         });
     });
 
-    const headerBtn = document.querySelector('.header__btn');
-    headerBtn.addEventListener('click', function(e) {
-        e.preventDefault();
-        const contacto = document.querySelector(e.target.attributes.href.value);
-        contacto.scrollIntoView( {
-            behavior: 'smooth',
-        });
-    });
+    // const headerBtn = document.querySelector('.header__btn');
+    // headerBtn.addEventListener('click', function(e) {
+        // e.preventDefault();
+        // const contacto = document.querySelector(e.target.attributes.href.value);
+        // contacto.scrollIntoView( {
+            // behavior: 'smooth',
+        // });
+    // });
 }
 
 // Hover imagen aboutme
@@ -40,3 +40,26 @@ imageContainer.addEventListener('mouseleave', () => {
   imageContainer.style.setProperty('--y', `50%`);
 });
 
+// Fade in al cargar
+window.addEventListener('DOMContentLoaded', () => {
+  document.body.classList.add('loaded');
+});
+
+// Fade out al cambiar de página
+document.querySelectorAll('a').forEach(link => {
+  if (
+    link.hostname === window.location.hostname &&
+    !link.hasAttribute('target') &&
+    !link.href.includes('#')
+  ) {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const href = this.getAttribute('href');
+
+      document.body.classList.remove('loaded'); // inicia fade out
+      setTimeout(() => {
+        window.location.href = href;
+      }, 500); // duración igual al CSS
+    });
+  }
+});
---
+++ proyectos/proyectos.js
@@ -0,0 +1,23 @@
+// Fade in al cargar
+window.addEventListener('DOMContentLoaded', () => {
+  document.body.classList.add('loaded');
+});
+
+// Fade out al cambiar de página
+document.querySelectorAll('a').forEach(link => {
+  if (
+    link.hostname === window.location.hostname &&
+    !link.hasAttribute('target') &&
+    !link.href.includes('#')
+  ) {
+    link.addEventListener('click', function (e) {
+      e.preventDefault();
+      const href = this.getAttribute('href');
+
+      document.body.classList.remove('loaded'); // inicia fade out
+      setTimeout(() => {
+        window.location.href = href;
+      }, 500); // duración igual al CSS
+    });
+  }
+});
